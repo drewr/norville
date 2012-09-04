22156e45420527c7e5460fe8ef4cba9ab8aeed4d
Drew Raines <aaraines@gmail.com>
2012-09-04 13:35:50 -0500

    Add patterns to prevent false positive route matches.

--- test/norville/test/middleware.clj
+++ test/norville/test/middleware.clj
@@ -2,7 +2,27 @@
   (:use [clojure.test])
   (:use [norville.middleware] :reload))
 
+(def __key :foo)
+
+(def client (wrap-explode-uri (fn [req] req) __key))
+
+(defn t [[meth uri] shrapnel]
+  (let [res (client {:ring {:request-method meth :uri uri}})
+        bool (= shrapnel (__key res))]
+    (if-not bool
+      (println ">>>>" res))
+    bool))
+
 (deftest t-explode
-  (let [client (wrap-explode-uri (fn [req] req) :foo)
-        expl (client {:ring {:request-method :get :uri "/blip/bop/1"}})]
-    (is (= (:foo expl) {:id "1", :type "bop", :index "blip"}))))
+  (is (t [:get  "/"                ] {}))
+  (is (t [:get  "/blip/bop/1"      ] {:index "blip", :type "bop", :id "1"}))
+  (is (t [:get  "/blip/_stats/get" ] {:index "blip"}))
+  (is (t [:get  "/blip/bop"        ] nil))
+  (is (t [:put  "/blip/bop/1"      ] {:index "blip", :type "bop", :id "1"}))
+  (is (t [:post "/blip"            ] {:index "blip"}))
+  (is (t [:put  "/blip"            ] {:index "blip"}))
+  (is (t [:put  "/blip/_mapping"   ] {:index "blip"}))
+  (is (t [:put  "/blip"            ] {:index "blip"}))
+  (is (t [:get  "/_cluster/health" ] {}))
+  (is (t [:get  "/_stats"          ] {}))
+  )
