51af03837079ab448692227670d606ceb5c04824
Lee Hinman <lee@writequit.org>
2012-11-12 11:58:29 -0700

    bump to a snapshot version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.draines/norville "1.0.0"
+(defproject com.draines/norville "1.0.1-SNAPSHOT"
   :description "Great Barrier Reef"
   :url "https://github.com/drewr/norville"
   :license {:name "Eclipse Public License"
